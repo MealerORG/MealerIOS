582191ea96ef0c41215509a602b65779a26bb8e2
Nelson Gou <nel4gou@gmail.com>
2022-07-12 22:14:40 -0700

    Add a Welcome View Controller

---
+++ Mealer/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  Mealer
+//
+//  Created by Nelson Gou on 7/12/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func continuePressed(_ sender: Any) {
+        print("pressed")
+    }
+}
