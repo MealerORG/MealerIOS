5336b09d01066d9d05c9144aa35a72b655964b40
Nelson Gou <nel4gou@gmail.com>
2022-07-13 18:37:41 -0700

    Finish Welcome screen and setup other screens

--- Mealer/WelcomeViewController.swift
+++ Mealer/WelcomeViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 class WelcomeViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.isNavigationBarHidden = true
+                
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
+                          NSAttributedString.Key.font: UIFont.systemFont(ofSize: 30, weight: .medium)]
+        let title = NSAttributedString(string: "Let's Go!", attributes: attributes)
+        button.setAttributedTitle(title, for: .normal)
+        button.layer.cornerRadius = 20
+    }
+    
     @IBAction func continuePressed(_ sender: Any) {
-        print("pressed")
+        performSegue(withIdentifier: "welcomeToApp", sender: self)
     }
 }
