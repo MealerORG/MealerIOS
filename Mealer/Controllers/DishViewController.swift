a2814dc79b7d1b933c0f68c47f936916f2330874
Nelson Gou <nel4gou@gmail.com>
2022-07-25 20:57:43 -0700

    Add custom table view cells to dish view

--- Mealer/Controllers/DishViewController.swift
+++ Mealer/Controllers/DishViewController.swift
@@ -20,6 +20,7 @@ class DishViewController: UIViewController {
         name.text = recipe?.name
         
         tableView.dataSource = self
+        tableView.register(UINib(nibName: "IngredientTableViewCell", bundle: nil), forCellReuseIdentifier: "DishIngredientCell")
     }
     
     @IBAction func donePressed(_ sender: UIButton) {
@@ -27,19 +28,18 @@ class DishViewController: UIViewController {
     }
 }
 
+// MARK: - Table View Data Source Methods
+
 extension DishViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return recipe?.ingredients.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DishIngredientCell", for: indexPath)
-        
-        if let ingredients = recipe?.ingredients, let quantities = recipe?.quantities {
-            var config = cell.defaultContentConfiguration()
-            config.text = "\(ingredients[indexPath.row]) — \(quantities[indexPath.row])"
-            cell.contentConfiguration = config
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DishIngredientCell", for: indexPath) as! IngredientTableViewCell
+                
+        cell.ingredient.text = recipe?.ingredients[indexPath.row]
+        cell.quantity.text = "\(recipe?.quantities[indexPath.row] ?? 0)"
         
         return cell
     }
---
+++ Mealer/Views/IngredientTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientTableViewCell.swift
+//  Mealer
+//
+//  Created by Nelson Gou on 7/25/22.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+    @IBOutlet weak var ingredient: UILabel!
+    @IBOutlet weak var quantity: UILabel!
+}
